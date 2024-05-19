19189879b1c8b2c6e023854a31141781706b3b1e
Sheddy00 <hei.sheddy.2@gmail.com>
2024-05-19 07:44:34 +0300

    Initial project

---
+++ src/test/java/hei/school/carshow/CarShowApplicationTests.java
@@ -0,0 +1,13 @@
+package hei.school.carshow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarShowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
